56c7d950cee1271d515c0e8ff0c1b336fe15fcc7
Josue <jreay@est.ups.edu.ec>
2021-12-30 18:41:32 -0500

    Proyecto
    paginas

---
+++ codigo/js/script_servicio.js
@@ -0,0 +1,10 @@
+
+function agregar() {
+    let nombre = document.getElementById('nombre');
+    let description = document.getElementById('nombre');
+    let tiempo = document.getElementById('nombre');
+    let valor = document.getElementById('nombre');
+    let tipo = document.getElementById('nombre');
+
+    
+}
